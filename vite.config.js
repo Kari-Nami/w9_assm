b66b69cb8f741de4ac43b5a037b4010a5f1c972b
kari-nami <kazakova.katya@outlook.com>
2026-08-17 12:54:59 +0700

    first dist

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/w9_assm/",
-  plugins: [react()],
-})
